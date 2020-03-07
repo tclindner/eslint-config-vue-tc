e8b4daaa76c166ed09869788bc266234131334e9
tclindner <tclindner@users.noreply.github.com>
2020-03-07 13:42:14 -0600

    Add vue/no-deprecated-slot-scope-attribute

--- rules/vue.js
+++ rules/vue.js
@@ -26,6 +26,7 @@ module.exports = {
     'vue/name-property-casing': ['error', 'kebab-case'],
     'vue/no-boolean-default': 'error',
     'vue/no-deprecated-slot-attribute': 'error',
+    'vue/no-deprecated-slot-scope-attribute': 'error',
     'vue/no-empty-pattern': 'error',
     'vue/require-direct-export': 'error',
     'vue/valid-v-slot': 'error',
