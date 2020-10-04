ded9262080533ed62c126fbc27a894a36d4cef5c
tclindner <tclindner@users.noreply.github.com>
2020-10-03 22:39:30 -0500

    Updates to eslint-plugin-vue
    Closes #158

--- rules/vue.js
+++ rules/vue.js
@@ -1,8 +1,6 @@
 module.exports = {
   rules: {
-    'vue/component-definition-name-casing': 'error',
     'vue/component-name-in-template-casing': 'error',
-    'vue/component-tags-order': 'error',
     'vue/dot-location': 'error',
     'vue/html-self-closing': [
       'warn',
@@ -28,24 +26,22 @@ module.exports = {
     // This is turned off for compatibility with Prettier
     'vue/singleline-html-element-content-newline': 'off',
     'vue/no-boolean-default': 'error',
-    // Add with v7 of eslint-plugin-vue when it is released.
-    // 'vue/no-custom-modifiers-on-v-model': 'error',
     'vue/no-deprecated-slot-attribute': 'error',
     'vue/no-deprecated-slot-scope-attribute': 'error',
+    'vue/no-empty-component-block': 'error',
     'vue/no-empty-pattern': 'error',
     'vue/no-irregular-whitespace': 'error',
-    // Add with v7 of eslint-plugin-vue when it is released.
-    // 'vue/no-multiple-template-root': 'error',
     'vue/no-reserved-component-names': 'error',
     'vue/no-static-inline-styles': 'error',
-    // Add with v7 of eslint-plugin-vue when it is released.
-    // 'vue/no-v-model-argument': 'error',
+    // Add once this is available in eslint-plugin-vue
+    // 'vue/no-template-no-target-blank': 'error',
+    'vue/no-unregistered-components': 'error',
+    'vue/no-unused-properties': 'error',
+    'vue/no-useless-mustaches': 'error',
+    'vue/no-useless-v-bind': 'error',
     'vue/padding-line-between-blocks': 'error',
     'vue/prop-name-casing': 'error',
     'vue/require-direct-export': 'error',
     'vue/require-name-property': 'error',
-    'vue/valid-v-bind-sync': 'error',
-    'vue/valid-v-slot': 'error',
-    'vue/v-slot-style': 'error',
   },
 };
