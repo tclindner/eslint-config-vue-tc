0ef391d1267a85cbbe0200b898218a4e62047dc5
Prashant Swami <prashant.swami@robustwealth.com>
2020-12-14 13:42:02 +0530

    refactor: added plugin as extension

--- index.js
+++ index.js
@@ -1,14 +1,13 @@
 const a11y = require.resolve('./rules/a11y');
 const vue = require.resolve('./rules/vue');
 const vuex = require.resolve('./rules/vuex');
-const testId = require.resolve('./rules/test-id');
 
 module.exports = {
-  extends: ['plugin:vue/recommended', a11y, vue, vuex, testId],
+  extends: ['plugin:vue/recommended', a11y, vue, vuex, 'plugin:test-id/recommended'],
   parserOptions: {
     parser: 'babel-eslint',
   },
-  plugins: ['vue', 'vuejs-accessibility', 'test-id'],
+  plugins: ['vue', 'vuejs-accessibility'],
   env: {
     es6: true,
   },
