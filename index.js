1a44e9981e827fa8794f14c4216691aff021d512
Prashant Swami <prashant.swami@robustwealth.com>
2020-12-15 11:44:43 +0530

    refactor: integrated as plugin

--- index.js
+++ index.js
@@ -1,13 +1,14 @@
 const a11y = require.resolve('./rules/a11y');
 const vue = require.resolve('./rules/vue');
 const vuex = require.resolve('./rules/vuex');
+const testId = require.resolve('./rules/test-id');
 
 module.exports = {
-  extends: ['plugin:vue/recommended', a11y, vue, vuex, 'plugin:test-id/recommended'],
+  extends: ['plugin:vue/recommended', a11y, vue, vuex, testId],
   parserOptions: {
     parser: 'babel-eslint',
   },
-  plugins: ['vue', 'vuejs-accessibility'],
+  plugins: ['vue', 'vuejs-accessibility', 'test-id'],
   env: {
     es6: true,
   },
